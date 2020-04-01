fe891d1de8e9141c2392f0b938671df8c8945d63
MoueedAhmed <moueedrao@gmail.com>
2020-04-02 01:00:23 +0500

    firebase serviceAccount.json path issue resolved

--- src/main/java/com/example/demo/service/FirebaseInitialize.java
+++ src/main/java/com/example/demo/service/FirebaseInitialize.java
@@ -3,10 +3,18 @@ package com.example.demo.service;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 
 @Service
 public class FirebaseInitialize {
@@ -14,8 +22,9 @@ public class FirebaseInitialize {
     @PostConstruct
     public void initialize() {
         try {
+
             FileInputStream serviceAccount =
-                    new FileInputStream("./serviceAccount.json");
+                    new FileInputStream("./src/main/resources/serviceAccount.json");
 
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
